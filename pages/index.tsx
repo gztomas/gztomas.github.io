f29d8fda1bfd740a9edfea589cfbf5cba392b138
Tomás González <tgonzalez@asapp.com>
2021-06-06 22:51:33 -0300

    feat: better look

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
+import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
+import normalize from "normalize.css/normalize.css";
 import { ReactElement } from "react";
 import { Github, LinkedIn, StackOverflow, Twitter } from "../src/icons";
 
+const ICON_SIZE = "24";
+
 const IndexPage = (): ReactElement => (
   <Root>
-    <H1>gztomas</H1>
-    <Footer>
-      <a href="https://twitter.com/_gztomas">
-        <Twitter width="20" />
-      </a>
-      <a href="https://www.linkedin.com/in/gztomas/">
-        <LinkedIn width="20" />
-      </a>
-      <a href="https://github.com/gztomas">
-        <Github width="20" />
-      </a>
-      <a href="https://stackoverflow.com/users/1216712/gztomas">
-        <StackOverflow width="20" />
-      </a>
-    </Footer>
+    <Global
+      styles={css`
+        @import url("https://fonts.googleapis.com/css2?family=Inconsolata:wght@200&display=swap");
+        ${normalize}
+      `}
+    />
+    <Splash>
+      <H1>gztomas</H1>
+      <Footer>
+        <a href="https://twitter.com/_gztomas">
+          <Twitter width={ICON_SIZE} />
+        </a>
+        <a href="https://www.linkedin.com/in/gztomas/">
+          <LinkedIn width={ICON_SIZE} />
+        </a>
+        <a href="https://github.com/gztomas">
+          <Github width={ICON_SIZE} />
+        </a>
+        <a href="https://stackoverflow.com/users/1216712/gztomas">
+          <StackOverflow width={ICON_SIZE} />
+        </a>
+      </Footer>
+    </Splash>
   </Root>
 );
 
@@ -34,14 +46,29 @@ const Root = styled.div`
   left: 0;
   background-color: #111212;
 `;
+const Splash = styled.div`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+  justify-content: right;
+`;
 const H1 = styled.h1`
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-family: Inconsolata, sans-serif;
+  font-size: 100px;
   color: white;
+  margin: 0;
 `;
 const Footer = styled.footer`
   display: flex;
-  width: 150px;
   justify-content: space-between;
+  margin-left: auto;
+
+  svg {
+    padding: 4px;
+    background-color: white;
+    margin: 8px;
+    border-radius: 4px;
+  }
 `;
 
 export default IndexPage;
